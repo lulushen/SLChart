de793c5d0d139c43b36477efeedbb35f608d4903
lulu.shen <lulu.shen@datacvg.com>
2018-12-26 15:35:58 +0800

    AppDelegate修改

---
+++ SLCharts/AppDelegate.h
@@ -0,0 +1,16 @@
+
+//
+//  SDAppDelegate.h
+//  SEMPMobile
+//
+//  Created by 上海数聚 on 16/8/1.
+//  Copyright © 2016年 上海数聚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
